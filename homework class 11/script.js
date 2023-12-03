a109a1fcb83ae9155fe3f1876ff5f4f741f5830a
Ir15260 <149706932+Ir15260@users.noreply.github.com>
2023-12-03 10:56:07 +0100

    Add files via upload

---
+++ homework class 11/script.js
@@ -0,0 +1,9 @@
+console.log("Homework Javascript");
+var phonePrice = 119.95;
+var tax = 0.05;
+var phoneNumber = 30;
+var phoneTax = phonePrice * tax;
+var phonePriceAfterTax = phonePrice + phoneTax;
+var result = phonePriceAfterTax * phoneNumber;
+
+console.log("The price of 30 phones after tax is: " + result + "$");
